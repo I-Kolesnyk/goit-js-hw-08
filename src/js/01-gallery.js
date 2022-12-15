e5233aaab18ca49637ef281a71657dc6cafc7ad9
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2022-12-15 02:12:44 +0200

    adds task 1 solving

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', stopDefaultActions);
+
+function makeGalleryMarkUp(images) {
+  return images.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+    ''
+  );
+}
+
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkUp(galleryItems));
+
+function stopDefaultActions(event) {
+  event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  fadeSpeed: 400,
+});
