1ee9b1676674676564ba4c0f2a16503e230eafe9
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2022-12-17 03:09:33 +0200

    Adds task 2 and 3 solving

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+
+const iframePlayer = new Player(iframeRef);
+
+iframePlayer.on('timeupdate', throttle(handlePlayerOn, 1000));
+iframePlayer.on('play', handlePlayerPlay);
+
+function handlePlayerOn({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+function handlePlayerPlay() {
+  iframePlayer.setCurrentTime(
+    localStorage.getItem('videoplayer-current-time') || 0
+  );
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', throttle(handleFormItemsInput, 500));
+formRef.addEventListener('submit', handleFormSubmit);
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+function handleFormItemsInput(event) {
+  if (event.target.name === 'email') {
+    formData.email = event.target.value;
+  } else if ((event.target.name = 'message')) {
+    formData.message = event.target.value;
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData || {}));
+}
+
+function handleFormSubmit(event) {
+  stopDefaultActions(event);
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+}
+
+(function addDataFromLocalStorage() {
+  const { email, message } = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (!{ email, message }) {
+    return;
+  }
+  formRef.elements.email.value = email;
+  formRef.elements.message.value = message;
+})();
+
+function stopDefaultActions(event) {
+  event.preventDefault();
+}
